8f3bc6338a6a3f559072b05b931bf02e61bc16c0
Jerome BOUHET <jerome.bouhet@maif.fr>
2026-01-20 16:50:56 +0100

    ex14

--- 14_utilityPartialRequired.ts
+++ 14_utilityPartialRequired.ts
@@ -27,17 +27,36 @@
 // TODO: Définir le type UserProfile
 
 export type UserProfile = {
-  // À compléter
+  username: string,
+  email: string,
+  age: number,
+  bio: string
 };
+
+const profile = { username: "john", email: "john@test.com", age: 25, bio: "Dev" };
 
 // TODO: Implémenter updateProfile
 
-export function updateProfile(profile: any, updates: any): any {
+export function updateProfile(profile: UserProfile, updates: Partial<UserProfile>): UserProfile {
+  const updateProfile = {
+    username: updates.username ?? profile.username,
+    email: updates.email ??  profile.email,
+    age: updates.age ?? profile.age,
+    bio: updates.bio ?? profile.bio
+  }
+  return updateProfile
+  // return { ...profile, ...updates };
+
   throw new Error("Not implemented");
 }
+console.log(updateProfile(profile, { age: 26 }))
 
 // TODO: Implémenter validateCompleteProfile
 
-export function validateCompleteProfile(profile: any): any {
+export function validateCompleteProfile(profile: Partial<UserProfile>): boolean {
+  // return (profile.username !== undefined && profile.email !== undefined && profile.age !== undefined && profile.bio !== undefined) ? true : false
+  return (profile.username !== undefined && profile.email !== undefined && profile.age !== undefined && profile.bio !== undefined)
   throw new Error("Not implemented") ;
 }
+console.log(validateCompleteProfile({ username: "john" }))
+console.log(validateCompleteProfile(profile))
